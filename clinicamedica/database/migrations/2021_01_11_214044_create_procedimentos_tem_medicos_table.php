b91435cf216f88c0d605b1df27e8d1c76a816ccd
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-01-11 17:44:01 -0400

    migrate estoque,secretaria;medico;procedimento_contem_medico

---
+++ clinicamedica/database/migrations/2021_01_11_214044_create_procedimentos_tem_medicos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProcedimentosTemMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('procedimentos_tem_medicos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('fk_procedimento');
+            $table->unsignedInteger('fk_medico');
+            $table->foreign('fk_medico')->references('id')->on('medicos');
+            $table->foreign('fk_procedimento')->references('id')->on('procedimentos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('procedimentos_tem_medicos');
+    }
+}
