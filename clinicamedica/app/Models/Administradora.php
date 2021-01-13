82e8394fb56ee5a6b05c30dd3f7ac5099fd6a744
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-01-13 16:29:32 -0400

    error no seeders

--- clinicamedica/app/Models/Administradora.php
+++ clinicamedica/app/Models/Administradora.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Administradora extends Model
 {
+    protected $table='administradoras';
     protected $fillable=['sexo', 'cidade', 'email', 'complemento', 'salario', 'cpf', 'rg', 'nome', 'longradouro', 'contato', 'bairro', 'uf', 'cep', 'datanascimento', 'foto'];
     
 }
--- clinicamedica/app/Models/Clinica.php
+++ clinicamedica/app/Models/Clinica.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Clinica extends Model
 {
-    //
+    protected $table='clinicas';
     protected $fillable=['responsaveltecnico', 'nome', 'logradouro', 'numero', 'cep', 'cidade', 'email', 'contato', 'foto', 'complemento', 'bairro', 'cnpj', 'uf'];
     
 
