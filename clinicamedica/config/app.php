8a6eb70359cffcb9b7197aff7b6775e4ae84772d
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-01-26 14:43:32 -0400

    horario de brasilia

--- clinicamedica/config/app.php
+++ clinicamedica/config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'America/Manaus',
+    'timezone' => 'America/Sao_Paulo',
 
     /*
     |--------------------------------------------------------------------------
