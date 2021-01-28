e0aea953f55f2495f9ff907c5b8b15e5093bc9ad
Élpis soluções digitais <elpissolucoesdigitais@gmail.com>
2021-01-28 09:33:58 -0400

    url fontwase + img padrão de user

--- clinicamedica/resources/views/templates/template.blade.php
+++ clinicamedica/resources/views/templates/template.blade.php
@@ -13,6 +13,7 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 		
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
         <link rel="stylesheet" href="{{url('assets/style.css')}}">
        
 </head>
@@ -27,7 +28,7 @@
             </div>
                   <div class="img bg-wrap text-center py-4" style="background-image: url(../assets/images/bg_1.jpg);">
                       <div class="user-logo">
-                          <div class="img" style="background-image: url(../assets/images/logo.jpg);"></div>
+                          <div class="img" style="background-image: url(../assets/images/user.PNG);"></div>
                           <h3>Admin</h3>
                       </div>
                   </div>
@@ -36,7 +37,8 @@
                 <a href="#"><span class="fa fa-home mr-3"></span> Home</a>
               </li>
               <li>
-                  <a href="#"><span class="fa fa-download mr-3 notif"><small class="d-flex align-items-center justify-content-center">5</small></span>Cadastro</a>
+                
+                  <a href="#"><span class="far fa-save mr-3"></span>Cadastro</a>
               </li>
               <li>
                 <a href="#"><span class="fa fa-gift mr-3"></span>Relatório</a>
